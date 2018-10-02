4c21e2349a56e6489eeb6fa045a2db84fe767cba
Andy <andy.peterson@my.wheaton.edu>
2018-10-02 14:38:16 -0500

    I added the Key item!

---
+++ game/Key.java
@@ -0,0 +1,22 @@
+package game;
+
+public class Key extends Item{
+
+	private Inventory pockets;
+	
+	public Key(Inventory pockets) {
+		this.pockets = pockets;
+	}
+	
+	public String description() {
+		return "Unlocks a mystery door";
+	}
+	
+	public String returnName() {
+		
+	}
+	
+	public void use() {
+		if ()
+	}
+}
