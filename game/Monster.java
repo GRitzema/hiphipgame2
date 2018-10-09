ed10413542cf91ade9bf3d24a290fe73baf8f50c
Andy <andy.peterson@my.wheaton.edu>
2018-10-09 15:36:53 -0500

    Done

---
+++ game/Monster.java
@@ -0,0 +1,18 @@
+package game;
+
+public class Monster {
+
+	private Riddle riddle;
+	private int health;
+	private Game game;
+	
+	public Monster(int health, Riddle riddle, Game game) {
+		this.health = health;
+		this.riddle = riddle;
+		this.game = game;
+	}
+	
+	public void combat() {
+		if ()
+	}
+}
