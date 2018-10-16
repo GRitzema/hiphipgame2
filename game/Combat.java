056cc20cc0b048b4e34115394d238f1c22415abd
Andy <andy.peterson@my.wheaton.edu>
2018-10-15 20:57:11 -0500

    Committed!

--- game/Combat.java
+++ game/Combat.java
@@ -4,8 +4,9 @@ public class Combat {
 
 	private Player p;
 	private Monster m;
+	private Inventory pockets;
 	
-	public Combat(Player p, Monster m) {
+	public Combat(Player p, Monster m, Inventory pockets) {
 		this.p = p;
 		this.m = m;
 	}
@@ -20,31 +21,44 @@ public class Combat {
 	public void playerCombat(Sword sword) {
 		double damage = 0;
 		if (Math.random()>=0.2) {
-			System.out.println("You missed" + m.getName() + "!");
+			System.out.println("You missed " + m.getName() + "!");
 		}
 		else {
 			if (sword.getMaterial().equals("wood")) {
 				damage = 10 + Math.random()*10;
 				m.setHealth(m.getHealth()-damage);
-				System.out.println(m.getName() + "took" + damage + "!");
+				System.out.println(m.getName() + "took " + damage + "!");
 			}
-			if (sword.getMaterial().equals("iron")) {
+			else if (sword.getMaterial().equals("iron")) {
 				damage = 25 + Math.random()*10;
 				m.setHealth(m.getHealth()-damage);
-				System.out.println(m.getName() + "took" + damage + "!");
+				System.out.println(m.getName() + "took " + damage + "!");
 			}
-			if (sword.getMaterial().equals("diamond")) {
+			else if (sword.getMaterial().equals("diamond")) {
 				damage = 50 + Math.random()*10;
 				m.setHealth(m.getHealth()-damage);
 				System.out.println(m.getName() + "took" + damage + "!");
 			}
+			else 
+				damage = 5 + Math.random()*10;
+				m.setHealth(m.getHealth()-damage);
+				System.out.println("Your fists inflicted " + damage + " on " + m.getName());
 		}
 	}
 	
 	
 	public void engage(String guess) {
 		if (guess.equals(m.getRiddle().getAnswer())){
-			
+			System.out.println("Hmmph, I suppose that answer will suffice.");
+			pockets.addToInventory(m.getKey());
+		}
+		else {
+			System.out.println("Mwahaha, your ignorance shall make you pay!");
+			while(m.getHealth() >= 0) {
+				playerCombat(p.getSword());
+				monsterCombat();
+			}
 		}
 	}
+	
 }
---
+++ game/Food.java
@@ -0,0 +1,37 @@
+package game;
+
+public class Food implements Item {
+
+	private int hp;
+	private String description;
+	private String name;
+	private Inventory pockets;
+	private Player p;
+	
+	public Food(int hp, String name, String description, Inventory pockets, Player p) {
+		this.hp = hp;
+		this.name = name;
+		this. description = description;
+		this.pockets = pockets;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	
+	public String returnName() {
+		return name;
+	}
+
+	
+	public void pickUp() {
+		pockets.addToInventory(this);	
+	}
+	
+	public void eat() {
+		p.setHealth(hp + p.getHealth());
+		System.out.println();
+	}
+	
+}
