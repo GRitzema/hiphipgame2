6a683368f2e1b930c942c7e51954f6006bc8e625
Grace <grace.ritzema@my.wheaton.edu>
2018-10-27 15:14:27 -0500

    lala

--- game/Inventory.java
+++ game/Inventory.java
@@ -52,15 +52,10 @@ public class Inventory {
 		
 			Object[] arr = pockets.keySet().toArray();
 			for (int j = 0; j<arr.length; j++) {
-<<<<<<< HEAD
-				System.out.println("    " + i + ". " + arr[j]);
-				i++;
-=======
 				if(!(((String) arr[j]).substring(0,6).equals("sphinx"))) {
 					System.out.println("    " + i + ". " + arr[j]);
 					i++;
 				}
->>>>>>> 1528d6dc1f521463f69beb91e9068c6aeb0175ac
 			}
 		}
 		else {
