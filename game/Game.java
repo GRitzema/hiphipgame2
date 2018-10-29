e86c23481a63e048f0d30adff5abc3043028a551
GRitzema <grace.ritzema@my.wheaton.edu>
2018-10-29 11:06:36 -0500

    fixed saga pantry

--- game/Game.java
+++ game/Game.java
@@ -118,9 +118,9 @@ public class Game {
         HashMap<String, Food> saga = new HashMap<String, Food>(); // The map of foods available
 		Shop sagaPantry = new Shop("Kitchen Pantry", saga, true); // The shop in room 7
 		sagaPantry.addToShop(new Food(5, "cookie", "Restores 5 hp", p, 1));
-		sagaPantry.addToShop(new Food(50,"salad","Restores all hp", p, 100));
+		sagaPantry.addToShop(new Food(50,"hearty salad","Restores all hp", p, 100));
 		sagaPantry.addToShop(new Food(35, "arancini", "Restores 35 hp", p, 10));
-		sagaPantry.addToShop(new Food(45, "curry", "Restores 45 hp", p, 20));
+		sagaPantry.addToShop(new Food(45, "spicy curry", "Restores 45 hp", p, 20));
 		sagaPantry.addToShop(new Food(25, "shin ramyun", "Restores 25 hp", p, 20));
 		rooms[7].setTheShop(sagaPantry);
 
